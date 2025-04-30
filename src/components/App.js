9dce2155f01488f767b3af4be9bdc14bd4a144d1
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-04-30 12:52:33 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   return (
     <div>
-       <p>Temperature: <span style={textStyle}>{weatherData.temperature}℃</span></p>
+       <p>Temperature: <span style={textStyle}>{weatherData.temperature}</span></p>
        <p>Conditions: <span>{weatherData.conditions}</span></p>
     </div>
   )
